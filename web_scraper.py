9a51833479e78a41d6ee825ab9554308f09141f9
kevincogan <44865890+kevincogan@users.noreply.github.com>
2020-08-20 14:51:16 +0100

    Add files via upload

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+
 
 #Gets the source code from the desired website and adds it to a file
 r = requests.get('https://coreyms.com/')
@@ -9,15 +11,17 @@ with open('website.txt', 'wb') as target:
 with open('website.txt', 'r') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
+csv_file = open('website_data.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Header', 'Summary', 'Link'])
+
 #Parses the video link from the website
 for article in soup.find_all('article'):
     headline = article.h2.a.text
     print(headline)
-    #print()
 
-    body_text = article.find('div', class_='entry-content')
-    print(body_text.p.text)
-    #print()
+    body_text = article.find('div', class_='entry-content').p.text
+    print(body_text)
 
     try:
         youtube_link = article.find('iframe', class_='youtube-player')['src']
@@ -31,6 +35,8 @@ for article in soup.find_all('article'):
 
     print()
 
+    csv_writer.writerow([headline, body_text, link])
+csv_file.close()
 
 
 #print(soup.prettify())
